e2faf99bd13e9dec0cc2559a8b0c887f37fbc49d
Grant TF <grant@tarrant-fisher.co.uk>
2022-03-02 09:25:57 +0000

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+
+<div class="text-center">
+
+<h1 class="text-3xl text-blue-500 font-bold underline">
+    Welcome to SvelteKit! <strong class="text-red-500 underline">(With Tailwind)</strong>
+  </h1>
+
+  <div class="container mx-auto px-20">
+  <div class="bg-zinc-50 rounded shadow border p-6 w-64">
+    <h5 class="text-2xl font-bold mb-4 mt-0">Tailwind Card</h5>
+    <p class="text-gray-700 text-sm">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+  </div>
+  </div>
+
+  <h2>Setup</h2>
+
+  <p>This was setup with:</p>
+  <ul>
+      <li>npx svelte-add@latest tailwindcss tailwind adder see <a href="https://github.com/svelte-add/tailwindcss">Tailwind adder</a></li>
+      <li>`npm init svelte@next my-app`</li>
+  </ul>
+</div>
